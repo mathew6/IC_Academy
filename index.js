4b0c064e973a24aecdd35c543d69739f5fed33e4
DylanFitzgerald <dfitz124@vt.edu>
2017-05-31 12:54:45 -0400

    added an autocomplete searchbar

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-var app = angular.module("topicList", []); 
-app.controller("myCtrl", function($scope) {
-    $scope.topics = [
-            {name:"Rockets", link:"https://en.wikipedia.org/wiki/Rocket" },
-            {name:"Satellite", link:"https://en.wikipedia.org/wiki/Satellite" }, 
-            {name:"Kepler", link: "https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion"}
-    ];
-    $scope.temp_topics = $scope.topics;
-    $scope.searchTopic = function() {
-        var ctr = 0;
-        for (var i = 0; i < $scope.topics.length; i++){
-            if ($scope.topics[i].name == $scope.searchMe){
-                $scope.temp_topics = [$scope.topics[i]];
-                console.log($scope.temp_topics);
-                ctr++;
-            }    
-        } 
-        if (!ctr) {
-            $scope.temp_topics = $scope.topics;
-        }
-    }
-});
+angular
+           .module('firstApplication', ['ngMaterial'])
+           .controller('autoCompleteController', autoCompleteController);
+
+        function autoCompleteController ($scope, $timeout, $q, $log) {
+           var self = this;
+           self.simulateQuery = false;
+           self.isDisabled    = false;
+           // list of states to be displayed
+           self.topics        = loadTopics();
+           self.querySearch   = querySearch;
+           self.selectedItemChange = selectedItemChange;
+           self.searchTextChange   = searchTextChange;
+           self.newState = newState;
+           function newState(state) {
+              alert("This functionality is yet to be implemented!");
+           }    
+           function querySearch (query) {
+              var results = query ? self.topics.filter( createFilterFor(query) ) : self.topics, deferred;
+              if (self.simulateQuery) {
+                 deferred = $q.defer();
+                 $timeout(function () { 
+                       deferred.resolve( results ); 
+                    }, 
+		            Math.random() * 1000, false);
+                 return deferred.promise;
+              } else {
+                 return results;
+              }
+           }
+           function searchTextChange(text) {
+              $log.info('Text changed to ' + text);
+           }
+           function selectedItemChange(item) {
+              $log.info('Item changed to ' + JSON.stringify(item));
+              if (item) {
+                var topic = item['display'];
+                var result = topic.link($scope.topics[topic]);
+                window.open($scope.topics[topic]);
+              }
+           }
+
+           function loadTopics() {
+              var allTopics = "Rocket, Satellite, Kepler";
+              $scope.topics = {"Rocket" : "https://en.wikipedia.org/wiki/Rocket",
+                               "Satellite" : "https://en.wikipedia.org/wiki/Satellite", 
+                               "Kepler" : "https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion"};
+              return allTopics.split(/, +/g).map( function (topic) {
+                 return {
+                    value: topic.toLowerCase(),
+                    display: topic
+                 };
+              });
+           }
+           //filter function for search query
+           function createFilterFor(query) {
+              var lowercaseQuery = angular.lowercase(query);
+              return function filterFn(state) {
+                 return (state.value.indexOf(lowercaseQuery) === 0);
+              };
+           }
+        }  
