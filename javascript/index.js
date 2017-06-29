dc4cd1eaaec6c166f33e5e3b5f2994b0bb50cf64
joshbarrientos1 <joshbarrientos1@gmail.com>
2017-06-29 14:13:50 -0400

    implement ui-route instead of ng-route

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,65 @@
-var app = angular.module('firstApplication', ['ngMaterial', 'ngRoute']);
+var app = angular.module('firstApplication', ['ngMaterial', 'ngRoute', 'ui.router']);
 
-app.config(['$routeProvider', function($routeProvider){
-    $routeProvider
-    .when('/', {
+//UI-router
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+        url: '/',
         templateUrl: 'templates/pages/home/index.html',
     })
-    .when('/topics', {
+    .state('topics', {
+        url: '/topics',
         templateUrl: 'templates/pages/topics/index.html',
     })
-    .when('/videos', {
+    .state('videos', {
+        url:'/videos',
         templateUrl: 'templates/pages/videos/index.html'
     })
-    .when('/about', {
+    .state('about', {
+        url: '/about',
         templateUrl: 'templates/pages/about/index.html'
     })
-    .when('/topics/orbital',{
+    .state('orbital',{
+        url: '/topics/orbital',
         templateUrl: 'templates/pages/topics/orbital/index.html'
     })
-    .when('/topics/satellites',{
+    .state('satellites',{
+        url: '/topics/satellites',
         templateUrl: 'templates/pages/topics/satellites/index.html'
     })
-    .when('/topics/kepler',{
+    .state('kepler',{
+        url: '/topics/kepler',
         templateUrl: 'templates/pages/topics/kepler/index.html'
     })
-    .otherwise({
-        redirectTo: '/'
+    .state('orbital.Lesson1', {
+        url: '/topics/orbital/lesson1',
+        templateUrl: 'templates/pages/topics/orbital/lesson1.html'
+    })
+    .state('orbital.Lesson2', {
+        url: '/topics/orbital/lesson2',
+        templateUrl: 'templates/pages/topics/orbital/lesson2.html'
+    })
+    .state('orbital.Lesson3', {
+        url: '/topics/orbital/lesson3',
+        templateUrl: 'templates/pages/topics/orbital/lesson3.html'
+    })
+    .state('orbital.Lesson4', {
+        url: '/topics/orbital/lesson4',
+        templateUrl: 'templates/pages/topics/orbital/lesson4.html'
+    })    
+    .state('orbital.Lesson5', {
+        url: '/topics/orbital/lesson5',
+        templateUrl: 'templates/pages/topics/orbital/lesson5.html'
+    })       
+    .state('orbital.Lesson6', {
+        url: '/topics/orbital/lesson6',
+        templateUrl: 'templates/pages/topics/orbital/lesson6.html'
+    })        
+});
+app.run(function($rootScope, $state, $document, $stateParams){ 
+    $rootScope.$state = $state; $rootScope.$stateParams = $stateParams;
+    $rootScope.$on('$stateChangeSuccess', function() { 
+        $document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
     });
-}]);
+})
