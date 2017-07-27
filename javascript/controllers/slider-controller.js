2180df8f0049a72af32a15e4f658987083ccad4b
DylanFitzgerald <dfitz124@vt.edu>
2017-07-27 15:43:47 -0400

    Remove unneeded stuff

--- javascript/controllers/slider-controller.js
+++ javascript/controllers/slider-controller.js
@@ -1,8 +1,8 @@
 angular.module('firstApplication').controller('sliderController', sliderController);
 
     function sliderController($scope, $log) {
-        $scope.test = 225;
-        $scope.test1 = 100;
+        $scope.test = 250;
+        $scope.test1 = 125;
         $scope.redLine = 575;
         $scope.blueLine = 150;
     };
